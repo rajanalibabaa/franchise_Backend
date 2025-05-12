6639f7c74caaa04ba247cca6cd50e056b931b9c0
rajanalibabaa <rajanalibabaa@gmail.com>
2025-05-12 10:43:02 +0530

    all main files to depeloy

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ import { BrandRegisterRoute } from "./src/Routes/BrandRoutes/BrandRegistorRoutes
 import { InvestorRouter } from "./src/Routes/InvestorRoutes/invRegisterRoutes.js";
 import { Login } from "./src/Routes/Login/LoginRoutes.js";
 import { logoutRouter } from "./src/Routes/Logout/logoutRoute.js";
+import sendOtpRouter from "./src/Routes/otpSenderRouter/sendOtp.js";
  
 
 const router = express.Router();
