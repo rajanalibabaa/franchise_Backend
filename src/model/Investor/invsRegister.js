f0cb8a2769d416eb1f375f6d3e675263556e7442
Pradeep123-backend-dev <pradeepbabaateam66@gmail.com>
2025-05-20 12:47:40 +0530

    Investor Register valid occupation Added

--- src/model/Investor/invsRegister.js
+++ src/model/Investor/invsRegister.js
@@ -47,7 +47,7 @@ const invsRegisterSchema = new mongoose.Schema(
 occupation: {
   type: String,
   required:  false,
-  enum: ["Business", "Professional", "Retired", "Student", "Other"] // Add valid options
+  enum: [ "Student", "Salaried Professional", "Bussiness Owner / Self-Employed","Retired","Freelancer/ Consultant","Homemaker","Investor","Other"] // Add valid options
 },
 specifyOccupation: {
   type: String,
