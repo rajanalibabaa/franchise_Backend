a52eec42d917d9a6b822e952ad63dd7b69a6dc6c
Aravind170425 <aravindbabaateam@gmail.com>
2025-05-28 16:45:21 +0530

    instant apply finished

--- src/model/Brand/brandFranchiseApply.js
+++ src/model/Brand/brandFranchiseApply.js
@@ -1,30 +1,32 @@
-// models/formSubmission.model.js
-
 import mongoose from 'mongoose';
 import uuid from '../../utils/uuid.js';
+
+
 const formSubmissionSchema = new mongoose.Schema(
   {
     uuid: {
       type: String,
-      default: uuid, // Ensure uuid is a function if it generates unique IDs
+      default: uuid,
       unique: true,
     },
     fullName: { type: String, required: true },
     location: { type: String, required: true },
-
     franchiseModel: { type: String, required: true },
     franchiseType: { type: String, required: true },
     investmentRange: { type: String, required: true },
     planToInvest: { type: String, required: true },
     readyToInvest: { type: String, required: true },
     brandId: { type: String, required: true },
-  brandName: { type: String }
+    brandName: { type: String },
+    brandEmail: { type: String },        // Added
+    investorEmail: { type: String ,required:true},     // Added
+    mobileNumber: { type: String,required:true},      // Added
   },
   {
-    timestamps: true, // adds createdAt and updatedAt
+    timestamps: true,
   }
 );
 
-const instaApplyBrnadForm = mongoose.model('instaApplyBrnadForm', formSubmissionSchema);
+const instaApplyBrandForm = mongoose.model('instaApplyBrandForm', formSubmissionSchema);
 
-export default instaApplyBrnadForm;
+export default instaApplyBrandForm;
