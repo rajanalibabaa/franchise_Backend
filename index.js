e3b6ffdab5b35088fb41b5bfeb643db7dd00d0ba
rajanalibabaa <rajanalibabaa@gmail.com>
2025-06-10 18:00:53 +0530

    main files

--- index.js
+++ index.js
@@ -11,23 +11,23 @@ import path from 'path';
 import s3Uploads from './src/Routes/s3Uploads/upload.js';
 import { getAllEndpoints } from './src/utils/endpoints/allEndPoints.js';
 import allRouters from './app.js';
-import { log } from 'console';
 
 const app = express();
 
 dotenv.config();
+app.use(cors( {
+    origin: ['https://foodbeverage.mrfranchise.in','http://localhost:5173'],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
 
+    allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With", "Accept", "Origin"],
+    
+    optionsSuccessStatus: 200,
+}));
 // Middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors( {
-    origin: ['https://foodbeverage.mrfranchise.in','http://localhost:5173'],
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
 
-    allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With", "Accept", "Origin", "Access-Control-Allow-Origin"],
-    credentials: true,
-    optionsSuccessStatus: 200,
-}));
 app.use(cookieParser());
 app.use(express.static(path.join(process.cwd(), 'public')));
 // app.use(bodyParser.json());
