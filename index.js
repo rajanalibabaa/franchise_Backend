15bbe21dbf5ff3c68c3ed4da31e5ecd943c06b2b
rajanalibabaa <rajanalibabaa@gmail.com>
2025-06-14 19:27:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const app = express();
 
 dotenv.config();
 app.use(cors( {
-    origin: ['https://foodbeverage.mrfranchise.in','http://localhost:5173'],
+    origin: ['https://foodandbeverage.mrfranchise.in','http://localhost:5173'],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
 
