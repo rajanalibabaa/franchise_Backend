58eece5248756935b291064a7785844c87c14674
Aravind170425 <aravindbabaateam@gmail.com>
2025-07-21 19:02:22 +0530

    push changes location lead

---
+++ src/Routes/Lead/LeadLocation.js
@@ -0,0 +1,10 @@
+// routes/admin/systemConfigRoutes.js
+import express from "express";
+import { updateSystemConfig,getSystemConfig } from "../../utils/All Leads/instantApplyLocationMatch";
+
+const router = express.Router();
+
+router.put("/system-config", updateSystemConfig); // Add auth middleware for admin if needed
+router.get("/system-config", getSystemConfig);
+
+export default router;
