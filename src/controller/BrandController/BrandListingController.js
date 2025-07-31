789f91f868b2d26794a123bc3680118d920dd904
rajanalibabaa <rajanalibabaa@gmail.com>
2025-07-31 12:07:15 +0530

    all main deploy files

--- src/controller/BrandController/BrandListingController.js
+++ src/controller/BrandController/BrandListingController.js
@@ -421,54 +421,7 @@ const getBrandListingByUUID = async (req, res) => {
   }
 };
 
-export const getTopFoodFranchise = async (req,res)=>{
-   try {
-    const topFranchises = await BrandFranchiseDetails.aggregate([
-      { $match : { "franchiseDetails.brandCategories.sub" :"Food Franchises"}},
-      
-      {$lookup: {
-        from: "branddetails",
-        localField: "brandOwnerId",
-        foreignField: "uuid",
-        as: "brandInfo"
-      }} ,
-      {$lookup: {
-        from: "branduploads",
-        localField: "brandOwnerId",
-        foreignField: "brandOwnerId", 
-        as: "uploads"
-      }},
-      {$project:{
-        // brandInfo: 1,
-        // franchiseDetails: 1,
-        // uploads: 1,
-        _id: 0,
-        brandId : "$brandInfo.brandID",
-        brandName : "$brandInfo.brandDetails.brandName",
-        brandCategory : "$franchiseDetails.brandCategories",
-        fico : "$franchiseDetails.fico",
-        logo: {
-          $cond: {
-            if: { $isArray: "$uploads.uploads.brandLogo" },
-            then: { $arrayElemAt: ["$uploads.uploads.brandLogo", 0] },
-            else: null
-          }
-        },
-        franchiseVideos: {
-          $ifNull: ["$uploads.uploads.franchisePromotionVideo", []]
-        },
-      }}
 
-    ])
-    if (!topFranchises || topFranchises.length === 0) {
-      return res.status(404).json(new ApiResponse(404, null, "No top food franchises found"));
-    }
-    return res.status(200).json(new ApiResponse(200, topFranchises, "Top food franchises fetched successfully"));
-   } catch (error) {
-    console.error(error);
-    return res.status(500).json({ message: "Server error" });
-   }
-}
 
 
 
@@ -737,6 +690,7 @@ const deleteBrandListingByUUID = async (req, res) => {
   }
 };
 
+// direct id find for  nner changes
 export const db = async (req, res) => {
   try {
     const data = await BrandListing.find({
@@ -764,6 +718,7 @@ export const db = async (req, res) => {
   }
 };
 
+// suffle re entry code 
 export const reEntry = async (req, res) => {
   try {
     const data = await BrandListing.find({});
@@ -855,6 +810,56 @@ export const allId = async(req,res) => {
    })
 
    return res.json(new ApiResponse(200,arr,"fetch successfully"))
+}
+
+
+export const getTopFoodFranchise = async (req,res)=>{
+   try {
+    const topFranchises = await BrandFranchiseDetails.aggregate([
+      { $match : { "franchiseDetails.brandCategories.sub" :"Food Franchises"}},
+      
+      {$lookup: {
+        from: "branddetails",
+        localField: "brandOwnerId",
+        foreignField: "uuid",
+        as: "brandInfo"
+      }} ,
+      {$lookup: {
+        from: "branduploads",
+        localField: "brandOwnerId",
+        foreignField: "brandOwnerId", 
+        as: "uploads"
+      }},
+      {$project:{
+        // brandInfo: 1,
+        // franchiseDetails: 1,
+        // uploads: 1,
+        _id: 0,
+        brandId : "$brandInfo.brandID",
+        brandName : "$brandInfo.brandDetails.brandName",
+        brandCategory : "$franchiseDetails.brandCategories",
+        fico : "$franchiseDetails.fico",
+        logo: {
+          $cond: {
+            if: { $isArray: "$uploads.uploads.brandLogo" },
+            then: { $arrayElemAt: ["$uploads.uploads.brandLogo", 0] },
+            else: null
+          }
+        },
+        franchiseVideos: {
+          $ifNull: ["$uploads.uploads.franchisePromotionVideo", []]
+        },
+      }}
+
+    ])
+    if (!topFranchises || topFranchises.length === 0) {
+      return res.status(404).json(new ApiResponse(404, null, "No top food franchises found"));
+    }
+    return res.status(200).json(new ApiResponse(200, topFranchises, "Top food franchises fetched successfully"));
+   } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Server error" });
+   }
 }
 
 
