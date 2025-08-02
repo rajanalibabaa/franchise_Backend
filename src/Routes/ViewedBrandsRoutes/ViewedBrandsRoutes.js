a8c87be2ebb2f51f3affbce76b9f3d68d5b758e2
rajanalibabaa <rajanalibabaa@gmail.com>
2025-08-02 11:15:12 +0530

    a

--- src/Routes/ViewedBrandsRoutes/ViewedBrandsRoutes.js
+++ src/Routes/ViewedBrandsRoutes/ViewedBrandsRoutes.js
@@ -9,6 +9,3 @@ ViewedBrandsRouter.post("/v1/view/postViewBrands/:id",verifyJWT,postViewBrands)
 ViewedBrandsRouter.get("/v1/view/getAllViewBrandByID/:id",verifyJWT,getAllViewBrandByID)
 ViewedBrandsRouter.delete("/v1/view/deleteViewBrandByID/:id",verifyJWT,deleteViewBrandByID)
 ViewedBrandsRouter.get("/v1/view/getAllViewBrands/:id",verifyJWT,getAllViewBrands)
-
-
-
