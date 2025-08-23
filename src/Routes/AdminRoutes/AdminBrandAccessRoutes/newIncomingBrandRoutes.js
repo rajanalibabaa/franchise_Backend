6c59b59f53f99d95851c48175a77a0ec5cf66c66
AyanMrFranchies <ayankalaibabaateam@gmail.com>
2025-08-23 16:45:37 +0530

    add new incoming brand routes and update brand model to support multiple controller admin brand access

---
+++ src/Routes/AdminRoutes/AdminBrandAccessRoutes/newIncomingBrandRoutes.js
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import { brandApprove, deleteBrandById, getNewIncomingBrandById, getNewIncomingBrands } from "../../../controller/Admin/Brand/newBrand.js";
+
+export const newIncomingBrandRouter = Router()
+
+newIncomingBrandRouter.get("/v1/admin/getNewIncomingBrands",getNewIncomingBrands)
+newIncomingBrandRouter.get("/v1/admin/getNewIncomingBrandById/:id",getNewIncomingBrandById)
+newIncomingBrandRouter.post("/v1/admin/brandApprove/:id",brandApprove)
+newIncomingBrandRouter.delete("/v1/admin/deleteBrandById/:id",deleteBrandById)
