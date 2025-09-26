35f81afc1a40da12f9c89607a00d26495b101b08
AyanMrFranchies <ayankalaibabaateam@gmail.com>
2025-09-26 10:08:18 +0530

    admin login route rearange

--- src/Routes/Login/LoginRoutes.js
+++ src/Routes/Login/LoginRoutes.js
@@ -6,8 +6,8 @@ const Login = Router()
 
 Login.post('/v1/login/',verifyLogin)
 Login.post('/v1/login/generateOTPforLogin',generateOTPforLogin)
-Login.post('/v1/login/generateOTPforAdminLogin',generateOTPforAdminLogin)
-Login.post('/v1/login/verifyAdminLoginOTP',verifyAdminLoginOTP)
+Login.post('/v1/admin/login/generateOTPforAdminLogin',generateOTPforAdminLogin)
+Login.post('/v1/admin/login/verifyAdminLoginOTP',verifyAdminLoginOTP)
 
 
 
