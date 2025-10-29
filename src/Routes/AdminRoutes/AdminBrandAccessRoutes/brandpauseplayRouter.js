3a6c515578bc66b53338d6f889917f79a5f4a1c2
AyanMrFranchies <ayankalaibabaateam@gmail.com>
2025-10-29 17:46:15 +0530

    add verification middleware

--- src/Routes/AdminRoutes/AdminBrandAccessRoutes/brandpauseplayRouter.js
+++ src/Routes/AdminRoutes/AdminBrandAccessRoutes/brandpauseplayRouter.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
 import { getAllPauseBrand, toggleBrandPausePlay } from "../../../controller/Admin/Brand/brandPausePlay.controller.js";
+import { verifyJWT } from "../../../Middleware/Authentication/authMiddleware.js";
 
 export const brandpauseplayRouter = Router()
 
-brandpauseplayRouter.post("/v1/admin/toggleBrandPausePlay/:id",toggleBrandPausePlay)
-brandpauseplayRouter.get("/v1/admin/getAllPauseBrand",getAllPauseBrand)
+brandpauseplayRouter.post("/v1/admin/toggleBrandPausePlay/:id",verifyJWT,toggleBrandPausePlay)
+brandpauseplayRouter.get("/v1/admin/getAllPauseBrand",verifyJWT,getAllPauseBrand)
--- src/controller/Admin/Brand/brandPausePlay.controller.js
+++ src/controller/Admin/Brand/brandPausePlay.controller.js
@@ -156,3 +156,4 @@ export const getAllPauseBrand = async (req, res) => {
     );
   }
 };
+  
