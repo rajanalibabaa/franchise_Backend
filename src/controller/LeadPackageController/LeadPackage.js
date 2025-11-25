bc9c67ac0545dce6c66631a86c285e1d5a0c6221
Aravind170425 <aravindbabaateam@gmail.com>
2025-11-25 10:38:48 +0530

    change date format

--- src/controller/LeadPackageController/LeadPackage.js
+++ src/controller/LeadPackageController/LeadPackage.js
@@ -19,7 +19,7 @@ export const format = (d) => {
 async function saveOldPackageToHistory(
   brand,
   packageEndTime,
-  packageStartTime
+  packageStartDate
 ) {
   if (!brand?.brandDetails?.paymentPackage) return;
 
@@ -32,7 +32,7 @@ async function saveOldPackageToHistory(
     perMonthLead: oldPkg.perMonthLead,
     totalLeads: oldPkg.totalLeads,
     isActive: !oldPkg.isActive,
-    packageStartTime: packageStartTime,
+    packageStartTime: packageStartDate,
     packageEndTime: packageEndTime,
     sentLeadsPercentage: oldPkg.sentLeadsPercentage,
     timestamp: format(new Date()),
@@ -134,7 +134,7 @@ export const leadPackageUpdate = async (req, res) => {
     const balanceLeads = PackageLeadCount - totalLeadCount;
 
     console.log("balanceLeads :", balanceLeads);
-    await saveOldPackageToHistory(brand, packageEndTime, packageStartTime);
+    await saveOldPackageToHistory(brand, packageEndTime, packageStartDate);
 
     if (upgradePacakgeType) {
       // Fetch the main payment packages document
