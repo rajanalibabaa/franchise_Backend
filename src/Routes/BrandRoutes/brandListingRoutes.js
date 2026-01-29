f10cc5342f2b69bcdbdcbfe5f2a84f24e5afe86a
rajanR14042000 <rajanravi144@gmail.com>
2026-01-29 12:33:52 +0530

    next js files to deploy the folder

--- src/Routes/BrandRoutes/brandListingRoutes.js
+++ src/Routes/BrandRoutes/brandListingRoutes.js
@@ -5,7 +5,7 @@ import {
   createBrandListing,
   deleteBrandListingByUUID,
   getAllBrands,
-  // getBrandListingByUUID,
+  getBrandListingByUUID,
   getBrandListingSlug,
   reEntry,
   updateBrandListingByUUID,
@@ -67,6 +67,8 @@ router.post(
 router.get("/v1/brandlisting/getAllBrandListing", getAllBrands);
 router.get("/v1/brandlisting/getBrandListingSlug/:identifier", getBrandListingSlug);
 router.get("/v1/brandlisting/getBrandById/:id",  getBrandById);
+
+router.get("/v1/brandlisting/getBrandListingByUUID/:id", getBrandListingByUUID);
 // router.get("/v1/brandlisting/getTopFoodFranchise", getTopFoodFranchise);
 // router.get("/v1/brandlisting/getTopBeverageFranchise", getTopBeverageFranchise);
 // router.get("/v1/brandlisting/getTopLeadingFranchise", getTopLeadingFranchise);
