6ee9478c3cdc1ca8b5692c87323be4ad788ae1c5
Aravind170425 <aravindbabaateam@gmail.com>
2026-05-05 15:47:21 +0530

    changes Schema

--- src/Routes/BrandPackagePlan/BrandPackagePlans.js
+++ src/Routes/BrandPackagePlan/BrandPackagePlans.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { createBrandPackage } from "../../controller/BrandPackagePlans/brandPackagePlans.js";
+import {createIntialPackages,updateBrandPackages} from "../../controller/BrandPackagePlans/brandPackagePlans.js";
 
 const brandPackagePlansRouter = express.Router();
 
 /* ================= CREATE PACKAGES ================= */
-brandPackagePlansRouter.post("/v1/brand-packages-plans/create", createBrandPackage);
+brandPackagePlansRouter.post("/v1/brand-packages-plans/create", createIntialPackages);
+brandPackagePlansRouter.put("/v1/brand-packages-plans/update", updateBrandPackages);
 // brandPackagePlansRouter.put("/v1/brand-packages-plans/upgrade", upgradeBrandPackages);
 export default brandPackagePlansRouter;
--- src/model/BrandPackagePlans/brandPackagePlans.js
+++ src/model/BrandPackagePlans/brandPackagePlans.js
@@ -9,7 +9,8 @@ const investmentRangeSchema = new mongoose.Schema(
     },
     selectedPlanState: [
       {
-        type: String
+        type: String,
+          default: []
       }
     ]
   },
@@ -23,7 +24,10 @@ const investmetPackageSchema = new mongoose.Schema(
       type: String
     },
 
-    investmentranges: [investmentRangeSchema],
+     investmentranges: {
+      type: [investmentRangeSchema],
+      default: []
+    },
 
     Validity: {
       type: String
@@ -79,7 +83,8 @@ const packageSchema = new mongoose.Schema(
     },
 
     planId: {
-      type: String
+      type: String,
+      required: true 
     },
 
     InvestmetPackages: [investmetPackageSchema]
@@ -103,7 +108,10 @@ const BrandPackagesSchema = new mongoose.Schema(
       type: String
     },
 
-    packages: [packageSchema]
+   packages: {
+      type: [packageSchema],
+      default: []
+    }
   },
   { timestamps: true }
 );
